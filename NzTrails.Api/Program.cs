2cb7ce433d6d498a0be831d442491430634aa330
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-27 22:19:51 -0600

    Injected NzWalksDb context into Program.cs

--- NzTrails.Api/Program.cs
+++ NzTrails.Api/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using NzTrails.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// add NzWalksDb context
+builder.Services.AddDbContext<NzWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NzWalksConnectionString"));
+});
 
 var app = builder.Build();
 
