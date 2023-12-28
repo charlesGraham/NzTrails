e10be6aa885bad6888334df4234e586d86ee8dd2
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-27 21:31:39 -0600

    Used dotnet aspnet-codegenerator to generate Student controller & tested Get route

---
+++ NzTrails.Api/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Leeland", "Dean", "Samantha" };
+            return Ok(studentNames);
+        }
+    }
+}
