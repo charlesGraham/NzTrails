f6449c12b5362c91db645fd74ab61fe6096a0049
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2024-01-03 10:29:10 -0600

    Configured Identity roles for NzWalksAuthDbContext

--- NzTrails.Api/Data/NzWalksAuthDbContext.cs
+++ NzTrails.Api/Data/NzWalksAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,5 +8,34 @@ namespace NzTrails.Api.Data
     {
         public NzWalksAuthDbContext(DbContextOptions<NzWalksAuthDbContext> options)
             : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // role IDs
+            var readerRoleId = "619d9b48-8c24-44b7-8181-00ca3f01a9e2";
+            var writerRoleId = "539877fd-5019-4118-ae20-0e0f9d4eda01";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
